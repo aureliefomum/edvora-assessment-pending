5568911a7c566358d927319a7f1158a9cb6c4ddd
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-12-22 22:41:08 -0300

    created dropdown menus

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import styles from "../../styles/Home.module.css";
-import Dropdown from "react-dropdown";
-import "react-dropdown/style.css";
+// import Dropdown from "react-dropdown";
+// import "react-dropdown/style.css";
 import { useEffect } from "react/cjs/react.development";
+import ProductDropDown from "./ProductDropDown";
+import StateDropDown from "./StateDropDown";
+import CitiesDropDown from "./CitiesDropDown";
 
 function Sidebar({
 	productsArr,
@@ -21,33 +24,21 @@ function Sidebar({
 				<div className={styles.filters}>Filters</div>
 
 				<div className={styles.dropdowns}>
-					{productsArr && (
-						<Dropdown
-							options={productsArr}
-							onChange={(ev) => {
-								setSelectedName(ev.value);
-							}}
-							value={productsArr[0]}
-						/>
-					)}
-					{statesArr && (
-						<Dropdown
-							options={statesArr}
-							onChange={(ev) => {
-								setSelectedState(ev.value);
-							}}
-							value={statesArr[0]}
-						/>
-					)}
-					{citiesArr && (
-						<Dropdown
-							options={citiesArr}
-							onChange={(ev) => {
-								setSelectedCity(ev.value);
-							}}
-							value={citiesArr[0]}
-						/>
-					)}
+					<ProductDropDown
+						productsArr={productsArr}
+						setSelectedName={setSelectedName}
+						selectedName={selectedName}
+					/>
+					<StateDropDown
+						statesArr={statesArr}
+						setSelectedState={setSelectedState}
+						selectedState={selectedState}
+					/>
+					<CitiesDropDown
+						citiesArr={citiesArr}
+						setSelectedCity={setSelectedCity}
+						selectedCity={selectedCity}
+					/>
 				</div>
 			</div>
 		</aside>
