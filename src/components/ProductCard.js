cf4350f1edeabfe2d6cb51aae4f9dac68720059d
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-12-22 22:36:42 -0300

    installed new dependency

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,24 +1,25 @@
 import styles from "../../styles/Home.module.css";
 
-function ProductCard({ data }) {
-	//console.log(data.data[1].address.state)
+function ProductCard({ product }) {
 	return (
 		<div className={styles.cardContainer}>
 			<div className={styles.topDiv}>
-				<img className={styles.img} src={data.data[1].image} />
+				{/* <img className={styles.img} src={product.image} /> */}
 				<div className={styles.topInfo}>
-					<div className={styles.productName}>Product Name</div>
-					<div className={styles.brandName}>Brand</div>
-					<div className={styles.productPrice}>Price</div>
+					<div className={styles.productName}>{product.product_name}</div>
+					<div className={styles.brandName}>{product.brand_name}</div>
+					<div className={styles.productPrice}>{product.price}</div>
 				</div>
 			</div>
 			<div className={styles.bottomDiv}>
 				<div className={styles.bottomInfo}>
-					<span className={styles.location}>Location</span>
-					<span className={styles.date}>Date</span>
+					<span
+						className={styles.location}
+					>{`${product.address.city}, ${product.address.state}`}</span>
+					<span className={styles.date}>{product.date}</span>
 				</div>
 
-				<span className={styles.description}>Description</span>
+				<span className={styles.description}>{product.discription}</span>
 			</div>
 		</div>
 	);
